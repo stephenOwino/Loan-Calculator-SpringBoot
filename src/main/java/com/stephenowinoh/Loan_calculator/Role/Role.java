baab5d3bfdca44c316be506a5893536093851804
stephenOwino <stephenowin233@gmail.com>
2025-01-31 14:52:23 +0300

    Aded @JsonValue annotation to Role enum for proper serialization as string

--- src/main/java/com/stephenowinoh/Loan_calculator/Role/Role.java
+++ src/main/java/com/stephenowinoh/Loan_calculator/Role/Role.java
@@ -1,8 +1,14 @@
 package com.stephenowinoh.Loan_calculator.Role;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Role {
         CUSTOMER,
-        ADMIN
-}
+        ADMIN;
 
+        // This method will ensure the enum is serialized as a string
+        @JsonValue
+        public String getRoleString() {
+                return this.name();
+        }
+}
